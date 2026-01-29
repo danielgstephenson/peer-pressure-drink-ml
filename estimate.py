8536923923fb8387a5e6c0b54f2238d7d4d797da
danielgstephenson <dgstephenson.econ@gmail.com>
2026-01-29 12:02:44 -0500

    stop time

--- estimate.py
+++ estimate.py
@@ -1,8 +1,11 @@
 from math import sqrt
+from numpy import tri
 import torch
 from torch import LongTensor, nn, Tensor
 import torch.nn.functional as F
 from torch.utils.data import random_split, TensorDataset, DataLoader, Subset
+import matplotlib.pyplot as plt
+
 from loader import target, treatment, covars
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -16,10 +19,12 @@ torch.set_printoptions(sci_mode=False)
 # setup optimization
 # setup training and testing data randomization
 # produce out of sample predictions
+# identify the early stopping time (Around 40)
 
 # TO DO:
-# identify the early stopping time (i.e. batch count)
+# FIX: THE MEAN OUTPUTS SEEM TO BE IDENTICAL FOR EACH TREATMENT (THEY SHOULD VARY BY TREATMENT)
 # measure treatment effects using counterfactal predictions
+# permutation test to identify significance
 # reconsider which variables to include (include only relevant variables)
 
 input_size = treatment.shape[1] + covars.shape[1]
@@ -48,7 +53,7 @@ class Model(nn.Module):
 
 dataset = TensorDataset(target, treatment, covars)
 
-def get_counterfactual_predictions(observation: int, trials = 100, batch_count= 50):
+def get_observation_path(observation: int, trial_count = 10, step_count= 50):
     test_dataset = Subset(dataset,[observation])
     train_dataset = Subset(dataset,[i for i in range(len(dataset)) if i != observation])
     test_dataloader = DataLoader(
@@ -61,42 +66,46 @@ def get_counterfactual_predictions(observation: int, trials = 100, batch_count= 
         batch_size=len(train_dataset),
         shuffle=True
     )
-    outputs = torch.zeros(4, trials).to(device)
-    for trial in range(trials):
+    test_loss_path = torch.zeros(step_count, trial_count).to(device)
+    test_output_path = torch.zeros(step_count, trial_count, 4).to(device)
+    for trial in range(trial_count):
         model = Model().to(device)
         optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
-        for batch in range(batch_count):
+        for step in range(step_count):
             (train_target, train_treatment, train_covars) = next(iter(train_dataloader))
             train_output = model(train_treatment,train_covars)
             train_loss = F.mse_loss(train_output, train_target)
             train_loss_value = train_loss.detach().cpu().numpy()
-            with torch.no_grad():
-                (test_target, test_treatment, test_covars) = next(iter(test_dataloader))
-                test_output = model(test_treatment, test_covars)
-                test_loss = F.mse_loss(test_output, test_target)
-                test_loss_value = test_loss.detach().cpu().numpy()
             train_loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            # print(f'batch: {batch}, loss: {train_loss_value:.5f}, test: {test_loss_value:.5f}')
-        (test_target, test_treatment, test_covars) = next(iter(test_dataloader))
-        test_target: Tensor = test_target
-        test_covars: Tensor = test_covars
-        x = Tensor(range(4)).long()
-        treatments = F.one_hot(Tensor(range(4)).long()).to(device)
-        targets = test_target.repeat(4,1).to(device)
-        covars = test_covars.repeat(4,1).to(device)
-        output = model(treatments, covars)
-        outputs[:,trial] = output[:,0]
-    predictions = torch.mean(outputs,dim=1)
-    return predictions
+            with torch.no_grad():
+                (test_target, test_treatment, test_covars) = next(iter(test_dataloader))
+                test_output = model(test_treatment, test_covars) # This is only using ONE treatment
+                # We need to get the predictions for all 4 treatments
+                test_loss = F.mse_loss(test_output, test_target)
+                test_loss_value = test_loss.detach().cpu().item()
+                test_loss_path[step, trial] = test_loss_value
+                test_output_path[step, trial, :] = test_output[:,0]
+            # print(f'step: {step}, loss: {train_loss_value:.5f}, test: {test_loss_value:.5f}')
+    observation_loss_path = torch.mean(test_loss_path,dim=1)
+    observation_output_path = torch.mean(test_output_path,dim=1)
+    return observation_output_path, observation_loss_path
 
-trials = 20
-batch_count = 20
-predictions = torch.zeros(len(dataset),1)
-treatment_numbers = torch.argmax(treatment,1)
-for i in range(len(dataset)):
-    print(f'observation {i+1} / {len(dataset)}')
-    counterfactual_predictons = get_counterfactual_predictions(i,trials,batch_count)
-    predictions[i,0] = counterfactual_predictons[treatment_numbers[i]]
-loss = F.mse_loss(predictions, target)
+trials = 2
+step_count = 40
+test_loss_matrix = torch.zeros(step_count, len(dataset)).to(device)
+test_output_matrix = torch.zeros(step_count, len(dataset), 4).to(device)
+for observation in range(len(dataset)):
+    output_path, loss_path = get_observation_path(observation,trials,step_count)
+    stop_time = torch.argmin(loss_path,dim=0)
+    test_loss_matrix[:,observation] = loss_path
+    test_output_matrix[:,observation,:] = output_path
+    print(f'observation: {observation+1} / {len(dataset)}, stop_time: {stop_time}')
+print(test_loss_matrix)
+mean_loss_path = torch.mean(test_loss_matrix,dim=1).cpu().numpy()
+mean_output_path = torch.mean(test_output_matrix,dim=1).cpu().numpy()
+mean_output_path[-1,:]
+
+plt.plot(mean_loss_path)
+plt.show()
