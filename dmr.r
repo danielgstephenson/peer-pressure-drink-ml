f983559d4adff4c5656c74323745c7ed7bb681f2
danielgstephenson <dgstephenson.econ@gmail.com>
2026-05-18 13:00:37 -0400

    plots

--- dmr.r
+++ dmr.r
@@ -1,12 +1,27 @@
 outcomeData = read.csv('output/outcome.csv')
 treatmentData = read.csv('output/treatment.csv')
+standardData = read.csv('data/standardData.csv')
+
+
+plot(outcomeData$outcome, outcomeData$output)
+hist(treatmentData$treatment,prob=TRUE)
+hist(treatmentData$output,prob=TRUE)
+mean(treatmentData$output[treatmentData$treatment==0])
+mean(treatmentData$output[treatmentData$treatment==1])
 
 summary(lm(outcome ~ output,data=outcomeData))
 summary(lm(treatment ~ output,data=treatmentData))
 
-outcome = outcomeData$outcome
-treatment = treatmentData$treatment
+mean(standardData$sd1[standardData$treatment==0])
+mean(standardData$sd1[standardData$treatment==1])
+
+outcome = standardData$outcome
+treatment = standardData$treatment
+setting1 = standardData$setting1
+setting2 = standardData$setting2
+setting3 = standardData$setting3
 summary(lm(outcome ~ treatment))
+summary(lm(outcome ~ treatment + setting1 + setting2 + setting3))
 
 outcomeResidual = outcomeData$outcome - outcomeData$output
 treatmentResidual = treatmentData$treatment - treatmentData$output
